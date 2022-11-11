f4599732fd319bf1480a8e5d74e6353cb69aae1b
TonyTostado <antoniobazalar.a@gmail.com>
2022-11-11 00:23:31 -0500

    Update

--- practice1.js
+++ practice1.js
@@ -6,3 +6,50 @@ const nuevoBool = true;
 const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40;
 const nuevoModulo = 21 % 5 === 1;
+
+//
+
+function devolverString(str){
+    return str;
+}
+console.log(devolverString); 
+
+//
+
+function devolverString2(a, b) {
+    var diferencia = a / b;
+    return diferencia;
+}
+devolverString2(6, 2);
+console.log(diferencia);
+
+//
+
+function sumarDosNumeros(x, y){
+    var suma = x + y;
+    return suma;
+}
+sumarDosNumeros(4, 2);
+
+//
+
+function sonIguales(x, y) {
+    if ( x === y){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+sonIguales(2, 3);
+
+//
+
+function combinarNombres(nombre, apellido){
+    var combinado = nombre + ' ' + apellido;
+    return combinado;
+}
+combinarNombres('Antonio', 'Bazalar');
+
+//
+
