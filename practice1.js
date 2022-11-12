7112b384ecefc9ea6ad906668a68e3d8f5b7acf7
TonyTostado <antoniobazalar.a@gmail.com>
2022-11-12 16:52:43 -0500

    Update

--- practice1.js
+++ practice1.js
@@ -53,3 +53,13 @@ combinarNombres('Antonio', 'Bazalar');
 
 //
 
+function esVocal(letra){
+if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  else if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
+}
+esVocal('a');
