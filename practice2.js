11369339f39c41c851711027a7943e852de3e08a
TonyTostado <antoniobazalar.a@gmail.com>
2022-11-19 13:58:32 -0500

    Update

--- practice2.js
+++ practice2.js
@@ -93,3 +93,19 @@ function esEntero(numero) {
     return false;
 }
 esEntero(4);
+
+function fizzBuzz(numero) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return 'fizzbuzz';
+    }
+    else if (numero % 3 === 0) {
+        return 'fizz';
+    }
+    else if (numero % 5 === 0) {
+        return 'buzz';
+    }
+    else {
+        return numero;
+    }
+}
+fizzBuzz(15);
