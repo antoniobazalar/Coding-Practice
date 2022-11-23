970a502335b423d46c75a541ac612cc4d1273826
TonyTostado <antoniobazalar.a@gmail.com>
2022-11-23 18:43:43 -0500

    Update

--- practice1.js
+++ practice1.js
@@ -7,6 +7,7 @@ nombre_usuario
 email002
 $ahorros
 // camelCase en JavaScript, escribir los identificadores de variables con minuscula en la primera palabra y empezar con mayuscula la segunda.
+// Nan significa 'Not a number'
 
 ////// Tipos de datos:
 // Strings
@@ -46,9 +47,55 @@ var a = 1, b = 2, c = 3;
 a = b = c;
 console.log(a, b, c); // 3
 
+// Operadores de comparación:
+== (igual a..)
+=== (igual a.. compara tambien el tipo de dato)
+!= (diferente a..)
+!== (diferente a.. compara tambien el tipo de dato)
+>= (mayor o igual que..)
+<= (menor o igual que..)
+> (mayor estricto que..)
+< (menor estricto que..)
+
+// Operadores logicos:
+// NOT (!) Devolvera el valor booleano opuesto de lo que se le pasa.
+!true;  //false
+!false; //true
+!(1 === 1); //false
+!(10 <= 9); //true
+// AND (&&) Devolvera true si y solamente si AMBAS expresione son true.
+true && true;   //true
+true && false;  //false
+false && true;  //false
+false && false; //false
+(100 > 10) && (10 === 10) //true
+(10 === 9) && (10 > 9)    //false
+// OR (||) Devolver'a true si al menos una de las expresiones o ambas son true. Devolvera false si AMBAS expresiones son false.
+true || true;   //true
+true || false;  //true
+false || true;  //true
+false || false; //false
+(100 > 10) || (10 === 10) //true
+(100 < 10) || (10 === 10) //true
+(100 > 10) || (10 !== 10) //true
+(10 === 9) || (10 < 9)    //false
+
+////// Metodos:
+// Realizan una accion en concreto solo para ciertos tipos de datos.
+var nombreGato = 'felix';
+console.log(nombreGato.length); // 5 Devuelve la cantidad de caracteres de esa cadena de strings.
+
+Math.pow(2,2);    // 4 Devuelve un numero elevado a un exponente.
+Math.round(6.5);  // 7 Redondea el numero entero mas cercano.
+Math.round(6.4);  // 7 
+Math.floor(6.99); // 6 Redondea al numero entero mas cercano hacia abajo.
+Math.ceil(6.01);  // 7 Redondea al numero entero mas cercano hacia arriba.
 
 
-/////////////////
+//////////////////////////////////
+//////////////////////////////////
+//////////////////////////////////
+
 
 const nuevaString = 'hola';
 const nuevoNum = 1996;
